e90f756a8f5931452595ef49d45b705432a796c7
Dushyant Khandelwal <dushyantkhandelwal4665@gmail.com>
2024-09-16 23:33:55 +0530

    add

---
+++ src/componets/First.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div className='bg-black'>
+
+        <div>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default First
