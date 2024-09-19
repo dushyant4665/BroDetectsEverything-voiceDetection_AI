10fc8be876028a7d63b9359a13ee9661c11e8a20
Dushyant Khandelwal <dushyantkhandelwal4665@gmail.com>
2024-09-19 23:56:08 +0530

    add

--- src/componets/First.tsx
+++ src/componets/First.tsx
@@ -1,19 +1,66 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 
 const First = () => {
+  const [listening, setListening] = useState(false);
+  const [transcript, setTranscript] = useState('');
+
+  let recognition;
+  if (typeof window !== 'undefined') {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = 'en-US';
+
+    recognition.onresult = (event) => {
+      let interimTranscript = '';
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+          setTranscript((prev) => prev + event.results[i][0].transcript);
+        } else {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+    };
+  }
+
+  const startListening = () => {
+    if (recognition) {
+      recognition.start();
+      setListening(true);
+    }
+  };
+
+  const stopListening = () => {
+    if (recognition) {
+      recognition.stop();
+      setListening(false);
+    }
+  };
+
   return (
-    <div className='bg-black'>
-
-       <div>
-
-    <p>heloo</p>
-  </div>
-      
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Simple Voice Recognition in Next.js</h1>
+      <button onClick={startListening} disabled={listening}>
+        Start Listening
+      </button>
+      <button onClick={stopListening} disabled={!listening}>
+        Stop Listening
+      </button>
+      <div
+        style={{
+          marginTop: '20px',
+          padding: '10px',
+          border: '1px solid black',
+          minHeight: '100px',
+        }}
+      >
+        <p>{transcript || 'Your speech will appear here...'}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default First
-
-
-
+export default First;
